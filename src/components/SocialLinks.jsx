dfffe8ceb9028fe4f847dfceef8f7d45b6871c1d
deadclown09 <mesandesh.2014@gmail.com>
2023-03-03 17:55:29 +0545

    Fixed a small bug related to links and redirects

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -12,7 +12,7 @@ const SocialLinks = () => {
           LinkedIn <FaLinkedin size={25} />
         </>
       ),
-      href: "https://linkedin.com/deadclown09",
+      href: "https://linkedin.com/in/deadclown09",
       style: "rounded-tr-md",
     },
     {
